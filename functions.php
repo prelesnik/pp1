e4fc5fabc7329133224d605317f1df8688f88b89
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-04 13:56:17 -0500

    added another function that determines the average of all the numbers in the given array

--- functions.php
+++ functions.php
@@ -1,6 +1,20 @@
 <?php
+    //find the largest number in the array
     function largest($array)
     {
         rsort($array);
         return $array[0];
+    }
+
+    //find the average of all numbers in the array
+    function average($array)
+    {
+        $total = 0;
+
+        for ($i = 0; $i < count($array); $i++)
+        {
+            $total += $array[$i];
+        }
+
+        return ($total / count($array));
     }
