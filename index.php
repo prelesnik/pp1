408247f07b32a460f546d48379ee4a1a67901de7
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-04 15:36:44 -0500

    added a call to a function that removes duplicates from an array

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
     include "functions.php";
 
     $numbers = array(7,9,8,9,8,8,6);
 
     function printArray($numbers)
     {
-        echo "<p>$numbers</p>";
+        echo "<p>" . implode($numbers, ", ") . "</p>";
     }
 ?>
 
@@ -18,11 +21,11 @@
 <body>
     <h1>Pair Programming Assignment 1</h1>
     <?php
-        echo implode($numbers, ", ");
-        echo "<br>";
-        echo largest($numbers);
-        echo "<br>";
-        echo average($numbers);
+        printArray($numbers);
+        echo "<p>" . largest($numbers) . "</p>";
+        echo "<p>" . average($numbers) . "</p>";
+        printArray(removeDups($numbers));
+        //print_r(distribution($numbers));
     ?>
 </body>
 </html>
