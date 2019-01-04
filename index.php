c264add52485c5250e3e471e680775e022959bfb
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-04 13:31:06 -0500

    changed file extention and added a function to print an array to the HTML document

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $numbers = array(7,9,8,9,8,8,6);
+
+    function printArray($numbers)
+    {
+        echo "<p>$numbers</p>";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PP1</title>
+</head>
+<body>
+    <h1>Pair Programming Assignment 1</h1>
+    <?php
+        echo implode($numbers, ", ");
+    ?>
+</body>
+</html>
